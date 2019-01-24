f827888b82e13d878c0dcfa5ee4a87690090b1f8
kuraus <e1739@s.akashi.ac.jp>
2019-01-24 10:23:26 +0900

    関数定義

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+//DatabaseUtilに実装する関数
+void WordRegister();
+void WordSearch();
+void Worddelete();
+
+//UserUtilに実装する関数
+void UseRegister();
+void UserCheck();
+void UserDelete();
+
+#endif
