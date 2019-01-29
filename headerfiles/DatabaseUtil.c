6d2680f3bb618255269bab490d86a5bef91eb22f
shinchan <shinchan3.14159265358979@gmail.com>
2019-01-29 22:34:47 +0900

    s

--- headerfiles/DatabaseUtil.c
+++ headerfiles/DatabaseUtil.c
@@ -1,57 +1,114 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-void WordSearch(){
+void WordSearch() {
 	FILE *fp;
-	char hoge[10];
-	fp=fopen("hoge.txt","r");
-	if(fp==NULL){
-		printf("hoge\n");
+	char word[100], search[100];
+	scanf("%s", search);
+	fp = fopen("hoge.txt", "r");
+	if (fp == NULL) {
+		printf("ないよぉ～～\n");
 		exit(1);
 	}
-	while(1){
-		fscanf(fp,"hoge = %s",hoge);
-		if()
-		if(feof(fp) != 0){
+	//あるかな？
+	while (1) {
+		if (fscanf(fp, "%s", word) == EOF) {
+			printf("ないよぉ～～\n");
 			break;
 		}
-		if()
+		if (strcmp(word, search) == 0) {
+			printf("あったよ\n");
+			break;
+		}
 	}
 	fclose(fp);
+	return;
 }
 
-void WordRegister(){
-	WordSearch();
+void WordRegister() {
+	int check = 0;
 	FILE *fp;
-	char hoge[10];
-	fp=fopen("hoge.txt","a+");
-	if(fp==NULL){
-		printf("hoge\n");
+	char word[100], search[100];
+	scanf("%s", search);
+	fp = fopen("hoge.txt", "r");
+	if (fp == NULL) {
+		printf("ないよぉ～～\n");
 		exit(1);
 	}
-	if(/*すでにあったら*/){
-		printf("hoge\n");
+	//あるかな？
+	while (1) {
+		if (fscanf(fp, "%s", word) == EOF) {
+			printf("ないよぉ～～\n");
+			break;
+		}
+		if (strcmp(word, search) == 0) {
+			printf("あったよ\n");
+			check++;
+			break;
+		}
+	}
+	fclose(fp);
+
+
+	FILE *fp;
+	char word[100];
+	fp = fopen("hoge.txt", "a");
+	if (fp == NULL) {
+		printf("ないよぉ～～\n");
 		exit(1);
-    }
-    fprintf(fp, "%s\n", hoge);
-    fclose(fp);
+	}
+	if (check == 1) {
+		printf("既に存在\n");
+		exit(1);
+	}
+	if (fprintf(fp, "%s\n", word) == 0) {
+		printf("しっぱ～い。もう少し頑張るドン!!\n");
+	}
+	printf("%sが登録されました.\n", word);
+	fclose(fp);
+	return ;
 }
-void WordDelete(){
-	WordSearch();
-	FILE *fp;
-	char hoge[10];
-	fp=fopen("hoge.txt","a+");
-	if(fp==NULL){
-		printf("hoge\n");
+
+void WordDelete() {
+	int check = 0;
+	char word[100], search[100];
+	FILE *fp, *tmp;
+
+	printf("消去したい言葉を入力してください >> ");
+	scanf("%s", search);
+
+	fp = fopen("hoge.txt", "r");
+	if (fp == NULL) {
+		printf("開けない\n");
 		exit(1);
 	}
-	if(){
-		fputs('\0',fp);
-		exit(1);
-    }
-    fprintf(fp, "%s\n", hoge);
-    fclose(fp);
+
+	tmp = fopen("copyhoge", "w");
+	while (fscanf(fp, "%s", word) != EOF) {
+		if (strcmp(word, search) != 0) {
+			fprintf(tmp, "%s\n", word);
+			check++;
+		}
+	}
+	fclose(fp);
+	fclose(tmp);
+	if (check == 0) {
+		printf("%sは登録されていません.\n\n", word);
+		return ;
+	}
+
+	fp = fopen("hoge", "w");
+	tmp = fopen("copyhoge", "r");
+	
+	while (fscanf(tmp, "%s", word) != EOF) {
+		fprintf(fp, "%s\n", word);
+	}
+	fclose(fp);
+	fclose(tmp);
+	printf("%sを消去しました。\n", search);
+	return ;
 }
 
 
