7d9bee72d155d31e31f33c16b1a9031740cf4a06
shinchan <32698751+murakamishinji@users.noreply.github.com>
2019-01-30 17:42:08 +0900

    DataBaseFileの見本を書いていただきたい

--- headerfiles/DatabaseUtil.c
+++ headerfiles/DatabaseUtil.c
@@ -3,23 +3,23 @@
 #include <string.h>
 #include <ctype.h>
 
+//単語検索
 void WordSearch() {
 	FILE *fp;
 	char word[100], search[100];
 	scanf("%s", search);
-	fp = fopen("hoge.txt", "r");
+	fp = fopen("DataBaseFile", "r");
 	if (fp == NULL) {
-		printf("ないよぉ～～\n");
+		printf("開けなかった\n");
 		exit(1);
 	}
-	//あるかな？
 	while (1) {
 		if (fscanf(fp, "%s", word) == EOF) {
-			printf("ないよぉ～～\n");
+			printf("ありませんでした\n");
 			break;
 		}
 		if (strcmp(word, search) == 0) {
-			printf("あったよ\n");
+			printf("%sを発見しました\n",word);
 			break;
 		}
 	}
@@ -27,65 +27,66 @@ void WordSearch() {
 	return;
 }
 
+//単語登録
 void WordRegister() {
-	int check = 0;
+	int check = 0;//既に登録されていれば1になる
 	FILE *fp;
 	char word[100], search[100];
 	scanf("%s", search);
-	fp = fopen("hoge.txt", "r");
+	fp = fopen("DataBaseFile", "r");
 	if (fp == NULL) {
-		printf("ないよぉ～～\n");
+		printf("開けませんでした\n");
 		exit(1);
 	}
-	//あるかな？
+	
 	while (1) {
 		if (fscanf(fp, "%s", word) == EOF) {
-			printf("ないよぉ～～\n");
 			break;
 		}
 		if (strcmp(word, search) == 0) {
-			printf("あったよ\n");
 			check++;
 			break;
 		}
 	}
 	fclose(fp);
-
+	//↑単語検索
 
 	FILE *fp;
 	char word[100];
-	fp = fopen("hoge.txt", "a");
+	fp = fopen("DataBaseFile", "a");
 	if (fp == NULL) {
-		printf("ないよぉ～～\n");
+		printf("開けませんでした\n");
 		exit(1);
 	}
 	if (check == 1) {
-		printf("既に存在\n");
+		printf("%sは既に登録されています\n",word);
 		exit(1);
 	}
 	if (fprintf(fp, "%s\n", word) == 0) {
-		printf("しっぱ～い。もう少し頑張るドン!!\n");
+		printf("失敗\n");
 	}
 	printf("%sが登録されました.\n", word);
 	fclose(fp);
 	return ;
 }
 
+//単語削除
 void WordDelete() {
-	int check = 0;
+	int check = 0;//単語があれば1
 	char word[100], search[100];
 	FILE *fp, *tmp;
 
 	printf("消去したい言葉を入力してください >> ");
 	scanf("%s", search);
 
-	fp = fopen("hoge.txt", "r");
+	fp = fopen("DataBaseFile", "r");
 	if (fp == NULL) {
-		printf("開けない\n");
+		printf("開けなかった\n");
 		exit(1);
 	}
 
-	tmp = fopen("copyhoge", "w");
+	//コピーのファイルを作る
+	tmp = fopen("CopyFile", "w");
 	while (fscanf(fp, "%s", word) != EOF) {
 		if (strcmp(word, search) != 0) {
 			fprintf(tmp, "%s\n", word);
@@ -99,8 +100,8 @@ void WordDelete() {
 		return ;
 	}
 
-	fp = fopen("hoge", "w");
-	tmp = fopen("copyhoge", "r");
+	fp = fopen("DataBaseFile", "w");
+	tmp = fopen("CopyFile", "r");
 	
 	while (fscanf(tmp, "%s", word) != EOF) {
 		fprintf(fp, "%s\n", word);
